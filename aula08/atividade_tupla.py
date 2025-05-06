e16a4e6ff5d10ba7f21ce7846524bb3df406fee3
gabriel-roza-curso <gabriel.roza@aluno.senac.rio>
2025-05-06 20:35:36 -0300

    Adicionando exercício

---
+++ aula08/atividade_tupla.py
@@ -0,0 +1,5 @@
+brasil = ("São Paulo", "Rio de Janeiro", "Distrito Federal", "Paraná")
+
+dados = ('João', 'Analista', 4000.00, 'Pedro', 'Vendedor', 2500.00)
+
+print(dados[0], dados[1], dados[2], dados[3])
---
+++ aula08/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+dados = ('João', 'Analista', 4000.00, 'Pedro', 'Vendedor', 2500.00)
+
+print(dados[0], dados[1], dados[2], dados[3])
